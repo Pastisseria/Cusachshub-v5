2bd1875de5ca18ff8ecbdeabdc4f506ad10b0d83
Pastisseria <pcusachs@gmail.com>
2026-09-20 18:34:01 +0200

    Activa detalle simplificado en Datos para la factura

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './presupuestoClienteExtra.js'
 import './dashboardEstadisticasCatering.js'
 import './presupuestoFacturadoControl.js'
 import './finalizarFacturacionPresupuesto.js'
+import './datosFacturaSimplificado.js'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthContext.jsx'
 

647d6b925c07ec64d7e18ed433f117c038564e31
Pastisseria <pcusachs@gmail.com>
2026-09-20 17:54:29 +0200

    Activa estadísticas de catering en Dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './presupuestoClienteExtra.js'
+import './dashboardEstadisticasCatering.js'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthContext.jsx'
 

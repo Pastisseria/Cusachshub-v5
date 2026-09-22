d842b006de3950cc6d162279e7c2ad8c71c86a4c
Pastisseria <pcusachs@gmail.com>
2026-09-22 16:48:37 +0200

    Activa nuevo diseño de Datos para la factura

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './datosFacturaPremium.css'
 import './presupuestoClienteExtra.js'
 import './dashboardEstadisticasCatering.js'
 import './presupuestoFacturadoControl.js'
